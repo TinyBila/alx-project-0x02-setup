5fe0c949056f66d8a48dc8488bda4fdca54902a1
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-26 21:55:29 +0200

    Create Card.tsx

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,9 @@
+import { CardProps } from "@/interfaces";
+export default function Card({ title, content }: CardProps) {
+  return (
+    <div className="border rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition">
+      <h2 className="text-xl font-semibold mb-2">{title}</h2>
+      <p className="text-gray-700">{content}</p>
+    </div>
+  );
+}
