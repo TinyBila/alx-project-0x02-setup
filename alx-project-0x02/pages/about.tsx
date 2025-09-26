718cec5d5cc683da36145058f96330dbf4360fb3
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-26 21:44:02 +0200

    Create about.tsx

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold text-green-600">About Page</h1>
+      <p className="mt-2 text-gray-700">
+        This project is built with Next.js, TypeScript, and Tailwind CSS.
+      </p>
+    </main>
+  );
+}
