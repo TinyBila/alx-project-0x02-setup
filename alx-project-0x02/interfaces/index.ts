2845274680fb33300e78140d1931f6b2b1727445
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-26 22:26:42 +0200

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -7,3 +7,16 @@ export interface User {
   title: string;
   content: string;
 }
+// interfaces/index.ts
+
+export interface CardProps {
+  title: string;
+  content: string;
+}
+
+export interface ButtonProps {
+  label: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  onClick?: () => void;
+}
