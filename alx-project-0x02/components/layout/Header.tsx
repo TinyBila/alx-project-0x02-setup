26034c027d13423cef2a9a010a089ae94b06b04f
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-26 21:29:17 +0200

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,15 @@
+import Link from "next/link";
 
+export default function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4 shadow">
+      <nav className="container mx-auto flex justify-between">
+        <h1 className="font-bold">ALX Project</h1>
+        <div className="space-x-4">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
